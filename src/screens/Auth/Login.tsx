81edd025349ac5880325920cb318987066144ab4
中村彦一郎 <hk.nakamura@gmail.com>
2025-05-26 21:41:38 +0900

    fix:  login04

--- src/screens/Auth/Login.tsx
+++ src/screens/Auth/Login.tsx
@@ -125,17 +125,17 @@ const Login: React.FC = () => {
             <button
               type="button"
               disabled={loading}
-              onClick={handleGoogleLogin}
+              //onClick={handleGoogleLogin}
               className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md bg-white text-sm font-medium text-gray-500 active:bg-gray-50 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              <span className="sr-only">Googleでログイン</span>
+              <span className="sr-only">Googleでログイン（一時無効）</span>
               <svg className="w-5 h-5" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
                   d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"
                 />
               </svg>
-              <span className="ml-2">Google</span>
+              <span className="ml-2">Google（準備中）</span>
             </button>
 
             <button
@@ -144,14 +144,14 @@ const Login: React.FC = () => {
               onClick={handleAppleLogin}
               className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md bg-white text-sm font-medium text-gray-500 active:bg-gray-50 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              <span className="sr-only">Appleでログイン</span>
+              <span className="sr-only">Appleでログイン（一時無効）</span>
               <svg className="w-5 h-5" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
                   d="M17.05 20.28c-.98.95-2.05.88-3.08.41-1.09-.47-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.41C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.19 2.31-.89 3.51-.84 1.54.07 2.7.61 3.44 1.57-3.14 1.88-2.29 5.13.22 6.41-.5 1.21-1.15 2.41-2.25 3.03zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"
                 />
               </svg>
-              <span className="ml-2">Apple</span>
+              <span className="ml-2">Apple（準備中）</span>
             </button>
           </div>
         </div>
